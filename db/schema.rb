0985fe38c00c693c5b9825272e4c13a538b585ce
Zuzanna Glowacka <zuzglowacka@gmail.com>
2017-11-10 16:12:46 +0000

    pushing everything to Heroku

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 20171109122030) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "cocktails", force: :cascade do |t|
     t.string   "name"
     t.datetime "created_at", null: false
@@ -24,8 +27,8 @@ ActiveRecord::Schema.define(version: 20171109122030) do
     t.integer  "ingredient_id"
     t.datetime "created_at",    null: false
     t.datetime "updated_at",    null: false
-    t.index ["cocktail_id"], name: "index_doses_on_cocktail_id"
-    t.index ["ingredient_id"], name: "index_doses_on_ingredient_id"
+    t.index ["cocktail_id"], name: "index_doses_on_cocktail_id", using: :btree
+    t.index ["ingredient_id"], name: "index_doses_on_ingredient_id", using: :btree
   end
 
   create_table "ingredients", force: :cascade do |t|
@@ -34,4 +37,6 @@ ActiveRecord::Schema.define(version: 20171109122030) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "doses", "cocktails"
+  add_foreign_key "doses", "ingredients"
 end
