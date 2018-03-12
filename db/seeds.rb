7c1c3595cc05b5b2244f2be798a99579db032642
Zuzanna Glowacka <zuzglowacka@gmail.com>
2018-03-12 16:37:47 +0000

    Puts Ruby version to Gemfile

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,7 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-Ingredient.destroy_all
+# Ingredient.destroy_all
 
 Ingredient.create(name: "lemon")
 Ingredient.create(name: "ice")
@@ -14,4 +14,20 @@ Ingredient.create(name: "tomato juice")
 Ingredient.create(name: "vodka")
 Ingredient.create(name: "Martini")
 Ingredient.create(name: "green olives")
-
+Ingredient.create(name: "pineapple")
+Ingredient.create(name: "sugar")
+Ingredient.create(name: "coconut cream")
+Ingredient.create(name: "Bacardi")
+Ingredient.create(name: "fresh pineapple juice")
+Ingredient.create(name: "Ice cubes")
+Ingredient.create(name: "freshly squeezed lemon juice")
+Ingredient.create(name: "cranberry juice")
+Ingredient.create(name: "orange juice")
+Ingredient.create(name: "mint springs")
+Ingredient.create(name: "gin")
+Ingredient.create(name: "tonic")
+Ingredient.create(name: "lime wedge")
+Ingredient.create(name: "coarse salt")
+Ingredient.create(name: "blanco tequila")
+Ingredient.create(name: "Cointreau")
+Ingredient.create(name: "fresh juice from 2 limes")
